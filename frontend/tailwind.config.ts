f97ebee58663b7b14395863da099869f39ae2993
ELEGANT-AXE\rizal <rizalfauzi774@gmail.com>
2026-02-17 22:34:05 +0700

    feat: Add Home page with profile, experience, and education sections, implement global styling with Tailwind CSS, and introduce a centralized alert system.

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,7 +4,12 @@ export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
