da1449b79c62c4a5dc1a7a7129c089e750d09ae1
ELEGANT-AXE\rizal <rizalfauzi774@gmail.com>
2026-02-17 22:34:24 +0700

    feat: Implement Home page with profile display and a global alert system for error handling.

---
+++ frontend/src/store/useAlertStore.ts
@@ -0,0 +1,35 @@
+// zustand
+import { create } from "zustand";
+
+export type AlertType = "success" | "error" | "warning" | "info";
+type AlertProps = {
+  title: string;
+  message: string;
+  type: AlertType;
+  duration: number;
+  isCenter: boolean;
+};
+
+interface AlertState {
+  alert: AlertProps | null;
+  setAlert: (alert: AlertProps) => void;
+  clearAlert: () => void;
+  showAlert: ({ message, type, title }: { message: string; type: AlertType; title: string }) => void;
+}
+
+export const useAlertStore = create<AlertState>((set) => ({
+  alert: null,
+  setAlert: (alert) => set({ alert }),
+  clearAlert: () => set({ alert: null }),
+  showAlert: ({ message, type, title }) => {
+    set({
+      alert: {
+        title,
+        message,
+        type,
+        duration: 5000,
+        isCenter: true,
+      },
+    });
+  },
+}));
